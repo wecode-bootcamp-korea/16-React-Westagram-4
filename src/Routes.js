376bd0efb797308c0db82cf4c5e566463f0bb1bd
Jaeyoon <sbjeong222@gmail.com>
2021-01-03 20:35:22 +0900

    Add: commit. 초기 세팅 이상증상 확인 후 처리 중 8시

---
+++ src/Routes.js
@@ -0,0 +1,28 @@
+// // 준식's 컴포넌트
+// import LoginSeonmi from './pages/seonmiJeong/Login/Login';
+// import MainSeonmi from './page/seonmiJeong/Main/Main';
+
+// // 종택's 컴포넌트
+// import loginBohyeon from './pages/bohyeonKoo/Login/Login';
+// import MainBohyeon from './pages/bohyeonKoo/Main/Main';
+
+// // 준식's 컴포넌트
+// import LoginKyuchan from './pages/kyuchanMun/Login/Login';
+// import MainKyuchan from './page/kyuchanMun/Main/Main';
+
+// // 종택's 컴포넌트
+// import LoginJaeyoon from './pages/jaeyoonJeong/Login/Login';
+// import MainJaeyoon from './pages/jaeyoonJeong/Main/Main';
+
+// // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
+// <Route exact path='/login-seonmi' component={LoginSeon} />
+// <Route exact path='/main-seonmi' component={MainSeon} />
+
+// <Route exact path='/login-bohyeon' component={LoginBo} />
+// <Route exact path='/main-bohyeon' component={MainBo} />
+
+// <Route exact path='/login-jaeyoon' component={LoginJae} />
+// <Route exact path='/main-jaeyoon' component={MainJae} />
+
+// <Route exact path='/login-kyuchan' component={LoginKyu} />
+// <Route exact path='/main-kyuchan' component={MainKyu} />
