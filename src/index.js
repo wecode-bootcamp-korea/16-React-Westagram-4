967098b0d0c6dd57500d0dae8174d297919364b0
Jaeyoon <sbjeong222@gmail.com>
2021-01-04 23:45:38 +0900

    Add: commit. index.js 코드 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-
+import reportWebVitals from './reportWebVitals';
 import './Styles/common.scss';
 import './Styles/reset.scss';
-ReactDOM.render(<Routes />, document.getElementById('root'));
 
+ReactDOM.render(<Routes />, document.getElementById('root'));
+reportWebVitals();
