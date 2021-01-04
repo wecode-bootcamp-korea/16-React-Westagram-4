2eeda9268d5596100f257cd10b4e51d188480ee3
Jaeyoon <sbjeong222@gmail.com>
2021-01-04 23:35:33 +0900

    Add: commit. images 폴더 생성, index.js 코드 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import reportWebVitals from './reportWebVitals';
+
 import './Styles/common.scss';
 import './Styles/reset.scss';
-ReactDOM.render(<Routes />,document.getElementById('root'));
-reportWebVitals();
+ReactDOM.render(<Routes />, document.getElementById('root'));
+
