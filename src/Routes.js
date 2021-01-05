6a98965b00513c8523a6b1c3430c758551237412
Jaeyoon <sbjeong222@gmail.com>
2021-01-05 20:36:31 +0900

    Routes 컴포넌트 이름 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,18 @@
 // 선미's 컴포넌트
-import LoginSeonmi from './pages/seonmiJeong/Login/Login';
-import MainSeonmi from './page/seonmiJeong/Main/Main';
+import LoginSeonmi from './Pages/seonmiJeong/Login/Login';
+import MainSeonmi from './Page/seonmiJeong/Main/Main';
 
 // 보현's 컴포넌트
-import loginBohyeon from './pages/bohyeonKoo/Login/Login';
-import MainBohyeon from './pages/bohyeonKoo/Main/Main';
+import LoginBohyeon from './Pages/bohyeonKoo/Login/Login';
+import MainBohyeon from './Pages/bohyeonKoo/Main/Main';
 
 // 규찬's 컴포넌트
-import LoginKyuchan from './pages/kyuchanMun/Login/Login';
-import MainKyuchan from './page/kyuchanMun/Main/Main';
+import LoginKyuchan from './Pages/kyuchanMun/Login/Login';
+import MainKyuchan from './Page/kyuchanMun/Main/Main';
 
 // 재윤's 컴포넌트
-import LoginJaeyoon from './pages/jaeyoonJeong/Login/Login';
-import MainJaeyoon from './pages/jaeyoonJeong/Main/Main';
+import LoginJaeyoon from './Pages/jaeyoonJeong/Login/Login';
+import MainJaeyoon from './Pages/jaeyoonJeong/Main/Main';
 
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route,} from 'react-router-dom';
@@ -25,8 +25,8 @@ class Routes extends React.Component {
         <Switch>
             <Route exact path='/login-seonmi' component={LoginSeonmi} />
             <Route exact path='/main-seonmi' component={MainSeonmi} />
-            <Route exact path='/login-bohyeon' component={LoginBohyun} />
-            <Route exact path='/main-bohyeon' component={MainBohyun} />
+            <Route exact path='/login-bohyeon' component={LoginBohyeon} />
+            <Route exact path='/main-bohyeon' component={MainBohyeon} />
             <Route exact path='/login-jaeyoon' component={LoginJaeyoon} />
             <Route exact path='/main-jaeyoon' component={MainJaeyoon} />
             <Route exact path='/login-kyuchan' component={LoginKyuchan} />
