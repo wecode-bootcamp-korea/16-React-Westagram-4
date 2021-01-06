da83048d42176380f99c09a354d8092027add30d
문규찬 <lazychan90@gmail.com>
2021-01-07 02:19:16 +0900

    [16기 문규찬] 로그인페이지 레이아웃 완료/ 로그인버튼 기능 구현중/ 메인페이지 레이아웃 작업중

--- src/Pages/kyuchanMun/Login/Login.js
+++ src/Pages/kyuchanMun/Login/Login.js
@@ -4,17 +4,53 @@ import {Link} from 'react-router-dom';
 
 
 class LoginKyuchan extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      id : "",
+      password : "",
+      hiddenPw : true,
+      productList : [],
+    };
+  }
+  handleIdChange = (e) => {
+    this.setState({ id : e.target.value});
+  };  
+  handlePasswordChange = (e) => {
+    this.setState({ password : e.target.value});
+  }; 
+  showPassword = (e) =>{
+    this.setState({hiddenPw : !this.state.hiddenPw});
+  }
+  
   render(){
+    const activBtn = (this.state.id.length && this.state.password.length) !=0
   return(
     <div className="Login">
     <section> 
         <div className="westagram">westagram</div>
         <div className="inputBox">
-            <input id="firstInput" type="text" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-            <input id="secondInput" type="password" placeholder="비밀번호"/>
-            <button type="button" id="btn"><Link to="/main-kyuchan">로그인</Link></button>
+            <input 
+                id="firstInput" 
+                type="text" 
+                placeholder="전화번호, 사용자 이름 또는 이메일" 
+                value={this.state.id}
+                onChange={this.handleIdChange}
+            />
+            <input 
+                id="secondInput" 
+                type={this.state.hiddenPw ? "password" : "text"}
+                placeholder="비밀번호"
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
+            />
+            <span className="show" onClick={this.showPassword}>Show!</span>
+            <button 
+                type="button" 
+                id="btn"
+                style={{backgroundColor : activBtn ? "black" : "#64b5f6" }}>로그인</button>
         </div>
-        <span>비밀번호를 잊으셨나요?</span>
+        <span><Link to="/main-kyuchan">비밀번호를 잊으셨나요?</Link></span>
     </section>
     </div>
   
